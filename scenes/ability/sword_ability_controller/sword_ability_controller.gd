5437adada03cce6dc7847c4e220b19b8c84a6d04
theWaffler <gjudianto@gmail.com>
2024-03-30 00:02:04 -0700

    targeting system

--- scenes/ability/sword_ability_controller/sword_ability_controller.gd
+++ scenes/ability/sword_ability_controller/sword_ability_controller.gd
@@ -1,5 +1,7 @@
 extends Node
 
+
+@export var max_range: float = 150
 @export var sword_ability: PackedScene
 
 # Called when the node enters the scene tree for the first time.
@@ -12,6 +14,19 @@ func on_timer_timeout():
 	if player == null:
 		return
 
+	var enemies = get_tree().get_nodes_in_group("enemy")
+	enemies = enemies.filter(func(enemy: Node2D):
+		return enemy.global_position.distance_squared_to(player.global_position) < pow(max_range, 2)
+	)
+	if enemies.size() == 0:
+		return
+	
+	enemies.sort_custom(func(a: Node2D, b: Node2D):
+		var a_distance = a.global_position.distance_squared_to(player.global_position)
+		var b_distance = b.global_position.distance_squared_to(player.global_position)
+		return a_distance < b_distance 
+	)
+
 	var sword_instance = sword_ability.instantiate() as Node2D
 	player.get_parent().add_child(sword_instance)
-	sword_instance.global_position = player.global_position
+	sword_instance.global_position = enemies[0].global_position
