744cb8aeed6e8c46c2e95cefc96e51b4e7998b0e
theWaffler <gjudianto@gmail.com>
2024-04-01 23:17:51 -0700

    blahh

--- scenes/ui/arena_time_ui.gd
+++ scenes/ui/arena_time_ui.gd
@@ -7,5 +7,10 @@ func _process(delta):
 	if arena_time_manager == null:
 		return
 
-	var time_elpased = arena_time_manager.get_time_eplased()
-	label.text = str(time_elpased)
+	var time_elpased = arena_time_manager.get_time_elapsed()
+	label.text = format_seconds_to_string(time_elpased)
+
+func format_seconds_to_string(seconds: float):
+	var minutes = floor(seconds/60) # round to nearest int value
+	var remaining_seconds = seconds - (minutes * 60)
+	return str(minutes) + ":" + ("%02d" % floor(remaining_seconds)) 
