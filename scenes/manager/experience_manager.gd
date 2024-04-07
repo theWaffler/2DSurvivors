17ecb1ebe5ae49fc296221d625ac47933ce4c55c
theWaffler <gjudianto@gmail.com>
2024-04-06 22:24:53 -0700

    added experience bar

--- scenes/manager/experience_manager.gd
+++ scenes/manager/experience_manager.gd
@@ -1,14 +1,28 @@
 extends Node
 
+signal experience_updated(current_exp: float, target_experience: float)
+
+const TARGET_EXPERIENCE_GROWTH = 5
+
 var current_exp = 0
+var current_level = 1
+var target_experience = 5
 
 func _ready():
 	GameEvents.experience_taco_collected.connect(on_exp_taco_collected)
 	
 
 func increment_exp(number: float):
-	current_exp += number
-	print(current_exp)
+	#urrent_exp += number
+	current_exp = min(current_exp + number, target_experience)
+	experience_updated.emit(current_exp, target_experience)
+	if current_exp == target_experience:
+		current_level += 1
+		target_experience += TARGET_EXPERIENCE_GROWTH
+		current_exp = 0 # reset current exp
+		experience_updated.emit(current_exp, target_experience) # emit updated exp
+	#else:
+	#	experience_updated.emit(current_exp, target_experience) # emit updated exp if not leveling up
 
 #signal handler
 func on_exp_taco_collected(number: float):
