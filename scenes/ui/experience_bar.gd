4f1125980c99d6ff7cba2165504295f4dfc129d5
theWaffler <gjudianto@gmail.com>
2024-04-06 22:30:49 -0700

    exp bar...wtf fr!?

--- scenes/ui/experience_bar.gd
+++ scenes/ui/experience_bar.gd
@@ -1,12 +1,15 @@
 extends CanvasLayer
 
 @export var experience_manager: Node
-@onready var progress_bar = $ProgressBar
+@onready var progress_bar = $MarginContainer/ProgressBar
 
 func _ready():
-	progress_bar.value = 0
-	experience_manager.experience_updated.connect(on_experience_updated)
+    progress_bar.value = 0
+    experience_manager.experience_updated.connect(on_experience_updated)
 
 func on_experience_updated(current_exp: float, target_experience: float):
-	var percent = current_exp / target_experience
-	progress_bar.value = percent
+    if target_experience == 0:
+        return
+        
+    var percent = current_exp / target_experience
+    progress_bar.value = percent
