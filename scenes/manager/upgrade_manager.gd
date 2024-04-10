93aa9276b2a8c7feda1bbafdfbb41bb8f25388a0
theWaffler <gjudianto@gmail.com>
2024-04-09 21:14:59 -0700

    Upgrade UI pop up on level up

--- scenes/manager/upgrade_manager.gd
+++ scenes/manager/upgrade_manager.gd
@@ -2,29 +2,36 @@ extends Node
 
 @export var upgrade_pool: Array[AbilityUpgrade]
 @export var experience_manager: Node
+@export var upgrade_screen_scene: PackedScene
 
 var current_upgrades = {
 
 }
 
 func _ready():
-    experience_manager.level_up.connect(on_level_up)
+	experience_manager.level_up.connect(on_level_up)
 
 
 func on_level_up(current_level: int):
-    var chosen_upgrade = upgrade_pool.pick_random() as AbilityUpgrade
+	var chosen_upgrade = upgrade_pool.pick_random() as AbilityUpgrade
+	if chosen_upgrade == null:
+		return
+	
+	var upgrade_screen_instance = upgrade_screen_scene.instantiate()
+	add_child(upgrade_screen_instance)
+	upgrade_screen_instance.set_ability_upgrade([chosen_upgrade] as Array[AbilityUpgrade]) 
+	
 
-    if chosen_upgrade == null:
-        return
-    
-    var has_upgrade = current_upgrades.has(chosen_upgrade.id)
-    if !has_upgrade:
-        current_upgrades[chosen_upgrade.id] = {
-            "resource": chosen_upgrade,
-            "quantity": 1
-        }
-    else:
-        current_upgrades[chosen_upgrade.id]["quantity"] += 1
-        
-    # debugging purpose
-    print(current_upgrades)
+func apply_upgrade(upgrade: AbilityUpgrade):
+
+	var has_upgrade = current_upgrades.has(upgrade.id)
+	if !has_upgrade:
+		current_upgrades[upgrade.id] = {
+			"resource": upgrade,
+			"quantity": 1
+		}
+	else:
+		current_upgrades[upgrade.id]["quantity"] += 1
+		
+	# debugging purpose
+	print(current_upgrades)
