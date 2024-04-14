44068b02c49e41cbd78fb57adac24d828eb3515f
theWaffler <gjudianto@gmail.com>
2024-04-14 00:43:35 -0700

    added upgrade ui selection and power up

--- scenes/ui/ability_upgrade_card.gd
+++ scenes/ui/ability_upgrade_card.gd
@@ -1,9 +1,18 @@
 extends PanelContainer
 
+signal selected
+
 @onready var name_label = $VBoxContainer/NameLabel
 @onready var description_label = $VBoxContainer/DescriptionLabel
+
+func _ready():
+	gui_input.connect(on_gui_input)
 
 # seperating logic from data. Accepts any AbilityUpgrade
 func set_ability_upgrade(upgrade: AbilityUpgrade):
 	name_label.text = upgrade.name
 	description_label.text = upgrade.description
+
+func on_gui_input(event: InputEvent):
+	if event.is_action_pressed("left_click"):
+		selected.emit()
