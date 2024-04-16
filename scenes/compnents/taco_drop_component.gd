411c68aa09af9783b7ab45314ba80d316d0e052e
theWaffler <gjudianto@gmail.com>
2024-04-16 00:55:51 -0700

    organized scene tree, so player and enemies will draw in the correct order

--- scenes/compnents/taco_drop_component.gd
+++ scenes/compnents/taco_drop_component.gd
@@ -20,5 +20,6 @@ func on_died():
 
     var spawn_position = (owner as Node2D).global_position
     var taco_instance = taco_scene.instantiate() as Node2D
-    owner.get_parent().add_child(taco_instance)
+    var entities_layer = get_tree().get_first_node_in_group("entities_layer")
+    entities_layer.add_child(taco_instance)
     taco_instance.global_position = spawn_position
