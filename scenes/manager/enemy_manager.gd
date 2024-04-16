cddde12b38f87d872d0d2f7851692aa073781ae4
theWaffler <gjudianto@gmail.com>
2024-04-16 01:00:50 -0700

    ug

--- scenes/manager/enemy_manager.gd
+++ scenes/manager/enemy_manager.gd
@@ -19,5 +19,9 @@ func on_timer_timeout():
 	var enemy = basic_enemy_scene.instantiate() as Node2D
 
 	var entities_layer = get_tree().get_first_node_in_group("entities_layer")
+
+	if entities_layer == null:
+		return
+	
 	entities_layer.add_child(enemy)
 	enemy.global_position = spawn_position
