94ebc914ddb2447a43801ccc46eb167859e2a780
theWaffler <gjudianto@gmail.com>
2024-04-17 22:24:54 -0700

    fixed tilemap issues and difficulty increasing function

--- scenes/manager/enemy_manager.gd
+++ scenes/manager/enemy_manager.gd
@@ -3,12 +3,19 @@ extends Node
 @export var SPAW_RADIUS = 330
 
 @export var basic_enemy_scene: PackedScene
+@export var arena_time_manager: Node
+@onready var timer = $Timer
+
+var base_spawn_time = 0
 
 func _ready():
-	$Timer.timeout.connect(on_timer_timeout)
+	base_spawn_time = timer.wait_time
+	timer.timeout.connect(on_timer_timeout)
+	arena_time_manager.arena_difficulty_increased.connect(on_arena_difficulty_increase)
 
 
 func on_timer_timeout():
+	timer.start()
 	var player = get_tree().get_first_node_in_group("player") as Node2D
 	if player == null:
 		return
@@ -25,3 +32,12 @@ func on_timer_timeout():
 	
 	entities_layer.add_child(enemy)
 	enemy.global_position = spawn_position
+
+func on_arena_difficulty_increase(arena_difficulty: int):
+	var time_off = (0.1/12) * arena_difficulty
+
+	time_off = min(time_off, 0.7)
+	#debug
+	print("Time off: " + str(time_off))
+
+	timer.wait_time = base_spawn_time - time_off
