6bbb3c91e5c3c15b7e0f8e88961e2b1579405f43
theWaffler <gjudianto@gmail.com>
2024-04-23 22:41:09 -0700

    added pickup mechanic for guitar weapon

--- scenes/manager/upgrade_manager.gd
+++ scenes/manager/upgrade_manager.gd
@@ -12,17 +12,6 @@ func _ready():
 	experience_manager.level_up.connect(on_level_up)
 
 
-func on_level_up(current_level: int):
-	var chosen_upgrade = upgrade_pool.pick_random() as AbilityUpgrade
-	if chosen_upgrade == null:
-		return
-	
-	var upgrade_screen_instance = upgrade_screen_scene.instantiate()
-	add_child(upgrade_screen_instance)
-	upgrade_screen_instance.set_ability_upgrade([chosen_upgrade] as Array[AbilityUpgrade]) 
-	upgrade_screen_instance.upgrade_selected.connect(on_upgrade_selected)
-	
-
 func apply_upgrade(upgrade: AbilityUpgrade):
 
 	var has_upgrade = current_upgrades.has(upgrade.id)
@@ -39,5 +28,29 @@ func apply_upgrade(upgrade: AbilityUpgrade):
 	# debugging purpose
 	#print(current_upgrades)
 
+
+func pick_upgrade():
+
+	var chosen_upgrades: Array[AbilityUpgrade]= []
+	var filtered_upgrades = upgrade_pool.duplicate() # pass by value. copy
+
+	for i in 2:
+		var chosen_upgrade = filtered_upgrades.pick_random() as AbilityUpgrade
+		chosen_upgrades.append(chosen_upgrade)
+		filtered_upgrades = filtered_upgrades.filter(func (upgrade): return upgrade.id != chosen_upgrade.id)
+
+	return chosen_upgrades
+
+
 func on_upgrade_selected(upgrade: AbilityUpgrade):
 	apply_upgrade(upgrade)
+
+
+func on_level_up(current_level: int):
+	var upgrade_screen_instance = upgrade_screen_scene.instantiate()
+	add_child(upgrade_screen_instance)
+
+	var chosen_upgrades = pick_upgrade()
+
+	upgrade_screen_instance.set_ability_upgrade(chosen_upgrades as Array[AbilityUpgrade]) 
+	upgrade_screen_instance.upgrade_selected.connect(on_upgrade_selected)
